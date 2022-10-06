d53564ff763396039dc6339e6fbed68871f159c9
Seth Abbey <frankbosconeven@gmail.com>
2022-10-06 16:37:43 +0000

    first commit

---
+++ pages/posts/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { getPosts } from '../../utils/getPosts'
+
+
+export async function getStaticProps() {
+    const posts = await getPosts();
+
+    return {
+        props: {
+            posts
+        },
+    };
+}
+  const Posts = ({posts}) => {
+  return (
+    <div>
+        {posts.map((post) => <p>{post.title}</p>
+            )
+        }
+    </div>
+  )
+}
+
+export default Posts
