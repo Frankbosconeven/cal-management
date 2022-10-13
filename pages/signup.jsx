8bd41cf4f5340385b0df30a06e73074abf98cfbc
Seth Abbey <frankbosconeven@gmail.com>
2022-10-13 20:57:20 +0000

    ad

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -106,7 +106,7 @@ const Signup = () => {
                 </div>
                 <span className="text-xs text-teal-400">Password must be same!</span>
                 <div className="flex">
-                    <Link href="/login">
+                    <Link href="/">
                     <button type='submit'
                     className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg 
                     hover:bg-blue-900">Create
